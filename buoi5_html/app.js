56b9aca0dfb390cc0a355f8c450e5853e8207a92
NGOCANH198 <88376657+NGOCANH198@users.noreply.github.com>
2021-08-23 21:27:05 +0700

    Add files via upload

---
+++ buoi5_html/app.js
@@ -0,0 +1,27 @@
+/*chúng ta sẽ hcoj được gì sau khi học html
++tự xây dựng được giao diện của 1 trang web
++phân tích được giao diện của website:
+-xây dựng layout: chia giao diện trang web bằng flexbug
+
+
+HTML: chính là ngôn ngữ và công cụ để tạo ra khun xương của 1 trang web
+ thẻ trong file html
+div,ul , li. , ing, h1,2,3,4,5   , p, input
+attribute : thuộc tính; sử dụng thuộc tính để thêm vào những
+thẻ h: dùng để viết tiêu đề;
+từ h1 -> h6: chữ sẽ giảm dần
+tìm hiểu các thẻ a,ul-li,table(th,tr,td),input,button
+<map>Thẻ HTML xác định một bản đồ hình ảnh. Bản đồ hình ảnh là một hình ảnh có các khu vực có thể nhấp được. 
+Các khu vực được xác định bằng một hoặc nhiều <area>thẻ.
+<img src="workplace.jpg" alt="Workplace" usemap="#workmap">
+Một khu vực có thể nhấp được xác định bằng cách sử dụng một <area>phần tử.
+<area shape="rect" coords="34, 44, 270, 350" href="computer.htm">
+Các <table>thẻ định nghĩa một bảng HTML.
+
+Mỗi hàng trong bảng được xác định bằng một <tr>thẻ. Mỗi tiêu đề bảng được xác định bằng một <th>thẻ. Mỗi ô / dữ liệu bảng được xác định bằng một <td>thẻ.
+
+Theo mặc định, văn bản trong <th>các phần tử được in đậm và căn giữa.
+
+Theo mặc định, văn bản trong <td>các phần tử là thông thường và được căn trái.
+block toàn màn hình
+inline: chỉ chứa bên trong text
